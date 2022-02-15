0b147625e7d188dc43a340e4d77354639974c7cd
kjw8319 <kjw8319@sjmnc.kr>
2022-02-15 12:05:47 +0900

    [TOUR OF TAILWIND 4.2 , 4.3] tailwindcss 이용한 간단한 div 카드 연습

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-10 grid gap-5">
+    <div className="bg-slate-400 py-20 px-20 grid gap-5 min-h-screen">
       <div className="bg-white p-6 rounded-3xl shadow-2xl">
         <span className="font-semibold text-3xl">Select Item</span>
         <div className="flex justify-between my-2">
@@ -21,8 +21,64 @@ const Home: NextPage = () => {
           Checkout
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
-      <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
+      <div className="bg-white overflow-hidden rounded-2xl shadow-2xl">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="rounded-3xl p-6 relative -top-5 bg-white">
+          <div className="flex relative -top-16 items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-gray-500 text-sm">Orders</span>
+              <span className="font-semibold">340</span>
+            </div>
+            <div className="w-24 h-24 bg-red-400 rounded-full" />
+            <div className="flex flex-col items-center">
+              <span className="text-gray-500 text-sm">Spent</span>
+              <span className="font-semibold">$2,310</span>
+            </div>
+          </div>
+          <div className="relative flex flex-col items-center -mt-10 -mb-5 ">
+            <span className="font-semibold text-lg">Tony Molloy</span>
+            <span className="text-sm text-gray-500">New York, USA</span>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-2xl shadow-2xl">
+        <div className="flex justify-between items-center mb-5">
+          <span>⬅️</span>
+          <div className="space-x-3">
+            <span>⭐ 4.9</span>
+            <span className="shadow-xl p-2 rounded-md">❤️</span>
+          </div>
+        </div>
+        <div className="bg-zinc-400 h-72 mb-5" />
+        <div className="flex flex-col">
+          <span className="font-semibold text-xl ">Swoon Lounge</span>
+          <span className="text-xs text-gray-500">Chair</span>
+          <div className="mt-3 mb-5 flex justify-between items-center">
+            <div>
+              <input type="radio" />
+              <input type="radio" />
+              <input type="radio" />
+            </div>
+            <div className="flex items-center space-x-5">
+              <button className=" bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500 rounded-lg">
+                -
+              </button>
+              <span>1</span>
+              <button className=" bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500 rounded-lg">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <span className="font-semibold text-2xl">$450</span>
+            <button className="bg-blue-500 text-white px-8 py-2 rounded-xl text-xs">
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="bg-white p-10 rounded-2xl shadow-2xl"></div>
     </div>
   );
