3060c1dd627bca9e79f979b764dc69f46c8c0926
kjw8319 <kjw8319@sjmnc.kr>
2022-02-16 15:08:38 +0900

    [#5 TAILWIND PRACTICE] 1. 로그인 창 HTML 및 간단한 css적용 2. 함수를 이용해 조건에 따라 tailwindcss 적용

---
+++ pages/practice/index.tsx
@@ -0,0 +1,42 @@
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const Home: NextPage = () => {
+  const router = useRouter();
+
+  const setPage = (link: string) => {
+    router.push('/practice' + link);
+  };
+
+  return (
+    <div className="flex flex-col items-center space-y-20">
+      <div
+        onClick={() => setPage('/cards')}
+        className="py-5 px-20 shadow-lg cursor-pointer hover:bg-teal-200 transition mt-72 group"
+      >
+        <span className={`font-bold  transition group-hover:text-white`}>
+          카드
+        </span>
+      </div>
+      <div
+        onClick={() => setPage('/files')}
+        className="py-5 px-20 shadow-lg cursor-pointer hover:bg-indigo-200 transition group"
+      >
+        <span>
+          <a className="font-bold transition group-hover:text-white">파일</a>
+        </span>
+      </div>
+      <div
+        onClick={() => setPage('/forms')}
+        className="py-5 px-20 shadow-lg cursor-pointer hover:bg-purple-200 transition group"
+      >
+        <span>
+          <a className="font-bold transition group-hover:text-white">인풋</a>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
