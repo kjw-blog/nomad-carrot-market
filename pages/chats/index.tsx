0ebc1aee46b04de5953d229fd6dabe0f7f8905d0
kjw8319 <kjw8319@sjmnc.kr>
2022-02-18 15:04:45 +0900

    [#5 TAILWIND PRACTICE] 1. 채팅 목록 페이지 html , css 적용 2. 채팅 내용 페이지 html , css 적용

---
+++ pages/chats/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next';
+
+// tw css divide = 형제 요소가 있을 때 구분선을 만들어줌 ??
+
+const Chats: NextPage = () => {
+  return (
+    <div className="py-10 divide-y-[1px]">
+      {[1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+        <div
+          key={i}
+          className="cursor-pointer px-4 flex space-x-3 items-center py-3 last:border-b-0"
+        >
+          <div className="h-10 w-10 bg-slate-300 rounded-full" />
+          <div>
+            <p className="text-gray-700">Steve Jebs</p>
+            <p className="text-sm text-gray-500">
+              See you tomorrow in the corner at 2pm!
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Chats;
