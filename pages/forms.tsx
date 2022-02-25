05359a8d2fc8b181ff9858c6d30b5ef628636749
kjw8319 <kjw8319@sjmnc.kr>
2022-02-25 15:17:57 +0900

    [#7 REACT HOOK FORM] 유효성 검사 에러처리와 각 오류 상황별 오류 메세지  저장

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -6,33 +6,48 @@
 // event에 대한 신경줄이기 ?
 // input 태그 간소화 (O)
 
-import { useForm } from 'react-hook-form';
+import { FieldErrors, useForm } from 'react-hook-form';
+
+interface LoginForm {
+  username: string;
+  email: string;
+  password: string;
+}
 
 export default function Form() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm<LoginForm>();
   /**
    *  register  : input 과 state를 연결해주는 다리역할
    *  watch     : form 안에 있는 value를 볼 수 있게함
    */
 
-  const onValid = () => {
+  const onValid = (data: LoginForm) => {
     console.log("i'm vaild bby");
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
   };
 
   return (
-    <form onSubmit={handleSubmit(onValid)}>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
-        {...register('username', { required: true })}
+        {...register('username', {
+          required: 'Username is required',
+          minLength: {
+            message: '5글자 이상을 입력해주세요.',
+            value: 5,
+          },
+        })}
         type="text"
         placeholder="Username"
       />
       <input
-        {...register('email', { required: true })}
+        {...register('email', { required: 'Email is required' })}
         type="email"
         placeholder="Email"
       />
       <input
-        {...register('password', { required: true })}
+        {...register('password', { required: 'Password is required' })}
         type="password"
         placeholder="Password"
       />
