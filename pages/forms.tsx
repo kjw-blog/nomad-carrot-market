b5ce57bb3b1e4c77ef6370f3aee3a9b6f0d29a40
kjw8319 <kjw8319@sjmnc.kr>
2022-02-25 14:14:36 +0900

    [#7 REACT HOOK FORM] useForm 을 이용한 간단한 유효성 검사 연습

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,6 +1,6 @@
 // == 원하는 수정 목록 ==
 // 코드 간소화 (O)
-// 더 나은 validation
+// 더 나은 validation  (o)
 // 더 나은 에러 처리 (set, clear, display)
 // input 태그에 대한 컨트롤
 // event에 대한 신경줄이기 ?
@@ -9,26 +9,32 @@
 import { useForm } from 'react-hook-form';
 
 export default function Form() {
-  const { register, watch } = useForm();
+  const { register, handleSubmit } = useForm();
   /**
    *  register  : input 과 state를 연결해주는 다리역할
    *  watch     : form 안에 있는 value를 볼 수 있게함
    */
 
+  const onValid = () => {
+    console.log("i'm vaild bby");
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onValid)}>
       <input
-        {...register('username')}
+        {...register('username', { required: true })}
         type="text"
         placeholder="Username"
-        required
       />
-      <input {...register('email')} type="email" placeholder="Email" required />
       <input
-        {...register('password')}
+        {...register('email', { required: true })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register('password', { required: true })}
         type="password"
         placeholder="Password"
-        required
       />
       <input type="submit" value={'Create Account'} />
     </form>
