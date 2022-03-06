a5ba106521f7868f9913db594707f257d316b7e9
DESKTOP-H3JKHHN\rkdwj <rkdwjddnr11@naver.com>
2022-03-06 20:09:07 +0900

    [#9 AUTHENTICATION] 프리즈마를 이용해서 유저 체크 및 생성

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,7 +3,61 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import client from '@libs/server/client';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+
+  // console.log({ test: 'test', ...payload });
+  // console.log({ test: 'test' }, payload);
+
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: 'Anonymous',
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
+
+  // if (email) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       email,
+  //     },
+  //   });
+  //   if (user) console.log('유저를 찾았습니다.');
+  //   if (!user) {
+  //     console.log('유저를 찾지못했습니다. 계정을 만듭니다.');
+  //     user = await client.user.create({
+  //       data: {
+  //         name: 'Anonymous',
+  //         email,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
+  // if (phone) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       phone: +phone,
+  //     },
+  //   });
+  //   if (user) console.log('유저를 찾았습니다.');
+  //   if (!user) {
+  //     console.log('유저를 찾지못했습니다. 계정을 만듭니다.');
+  //     user = await client.user.create({
+  //       data: {
+  //         name: 'Anonymous',
+  //         phone: +phone,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
+
   return res.json({ ok: true });
 }
 
