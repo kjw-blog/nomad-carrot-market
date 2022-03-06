1f76433dedf0f8a7d430852f6fd384ee45b29c0f
DESKTOP-H3JKHHN\rkdwj <rkdwjddnr11@naver.com>
2022-03-06 21:09:50 +0900

    [#9 AUTHENTICATION] 1. api response의 타입 지정 2. 토큰 payload에 들어갈 랜덤한 숫자 생성 3. twilio를 사용하기 위한 코드 env 추가

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -3,9 +3,14 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
 
+export interface ResponseType {
+  ok: boolean;
+  [key: string]: any;
+}
+
 export default function withHandler(
   method: 'GET' | 'POST' | 'DELETE',
-  fn: (req: NextApiRequest, res: NextApiResponse) => void
+  fn: (req: NextApiRequest, res: NextApiResponse<ResponseType>) => void
 ) {
   return async function (req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== method) {
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,68 +1,33 @@
-import withHandler from '@libs/server/withHandler';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
 import client from '@libs/server/client';
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { phone, email } = req.body;
-  const payload = phone ? { phone: +phone } : { email };
-
-  // console.log({ test: 'test', ...payload });
-  // console.log({ test: 'test' }, payload);
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + '';
 
   const token = await client.token.create({
     data: {
-      payload: '1234',
+      payload,
       user: {
         connectOrCreate: {
           where: {
-            ...payload,
+            ...user,
           },
           create: {
             name: 'Anonymous',
-            ...payload,
+            ...user,
           },
         },
       },
     },
   });
   console.log(token);
-
-  // if (email) {
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       email,
-  //     },
-  //   });
-  //   if (user) console.log('유저를 찾았습니다.');
-  //   if (!user) {
-  //     console.log('유저를 찾지못했습니다. 계정을 만듭니다.');
-  //     user = await client.user.create({
-  //       data: {
-  //         name: 'Anonymous',
-  //         email,
-  //       },
-  //     });
-  //   }
-  //   console.log(user);
-  // }
-  // if (phone) {
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       phone: +phone,
-  //     },
-  //   });
-  //   if (user) console.log('유저를 찾았습니다.');
-  //   if (!user) {
-  //     console.log('유저를 찾지못했습니다. 계정을 만듭니다.');
-  //     user = await client.user.create({
-  //       data: {
-  //         name: 'Anonymous',
-  //         phone: +phone,
-  //       },
-  //     });
-  //   }
-  //   console.log(user);
-  // }
 
   return res.json({ ok: true });
 }
