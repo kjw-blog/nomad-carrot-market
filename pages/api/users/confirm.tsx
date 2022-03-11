70ea7d84fb4f3da92d301d0212c801a9d0de822b
DESKTOP-H3JKHHN\rkdwj <rkdwjddnr11@naver.com>
2022-03-11 17:01:29 +0900

    [#9 AUTHENTICATION] 1. 토큰 인증창에 iron session 적용

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,3 +1,4 @@
+import { withIronSessionApiRoute } from 'iron-session/next';
 import client from '@libs/server/client';
 import withHandler, { ResponseType } from '@libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
@@ -7,7 +8,29 @@ async function handler(
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
+  const exists = await client.token.findUnique({
+    where: { payload: token },
+    // include: { user: true },
+    // include 로 해당 model에 관련된 model의 data를 가져올수있다
+  });
+  if (!exists) res.status(404).end();
+  req.session.user = {
+    id: exists?.userId,
+  };
+  // userId로 세션을 만든다
+
+  await req.session.save();
+  // 해당 세션을 저장한다
+
   res.status(200).end();
 }
 
-export default withHandler('POST', handler);
+export default withIronSessionApiRoute(withHandler('POST', handler), {
+  cookieName: 'carrotsession',
+  password: '1238105534509238234253456726735674',
+});
+
+/**
+ *  cookieName : 생성할 쿠키의 이름
+ *  password : 쿠키를 암호화할때 사용할 비밀번호
+ */
