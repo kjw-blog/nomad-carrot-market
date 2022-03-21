ac500b99796974a949de3ae799c0f8bb7e706919
kjw-blog <rkdwjddnr11@naver.com>
2022-03-21 16:23:58 +0900

    [#11 PRODUCTS] 1. Fav model 추가 2. 상품 좋아요 버튼 api 생성

---
+++ pages/api/products/[id]/fav.ts
@@ -0,0 +1,52 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { withApiSession } from '@libs/server/withSession';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
+import client from '@libs/server/client';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+
+  const alreadyExists = await client.fav.findFirst({
+    where: {
+      productId: +id.toString(),
+      userId: user?.id,
+    },
+  });
+  if (alreadyExists) {
+    await client.fav.delete({
+      where: {
+        id: alreadyExists.id,
+      },
+    });
+  } else {
+    await client.fav.create({
+      data: {
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+        product: {
+          connect: {
+            id: +id.toString(),
+          },
+        },
+      },
+    });
+  }
+
+  res.status(200).json({ ok: true });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ['POST'],
+    handler,
+  })
+);
