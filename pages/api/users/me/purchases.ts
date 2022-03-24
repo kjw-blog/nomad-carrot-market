3f7e4ffb47bb0cd8228787672bd0565f8a2be24c
kjw-blog <rkdwjddnr11@naver.com>
2022-03-24 15:33:49 +0900

    [#13 PROFILE] 판매내역, 구매내역, 관심목록 리스트를 가져오는 api 생성

---
+++ pages/api/users/me/purchases.ts
@@ -0,0 +1,31 @@
+import withHandler, { ResponseType } from '@libs/server/withHandler';
+import { withApiSession } from '@libs/server/withSession';
+import { NextApiRequest, NextApiResponse } from 'next';
+import client from '@libs/server/client';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    session: { user },
+  } = req;
+
+  const purchases = await client.purchase.findMany({
+    where: {
+      userId: user?.id,
+    },
+    include: {
+      product: true,
+    },
+  });
+
+  res.status(200).json({ ok: true, purchases });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ['GET'],
+    handler,
+  })
+);
