857daecd28a1a7872fb760c23c0a1dcbd720835f
kjw8319 <kjw8319@sjmnc.kr>
2022-05-04 12:08:52 +0900

    [#15 CLOUDFLARE IMAGES] 프로필 변경에서 프로필사진 변경 시 변경될 이미지를 미리 보여지도록 수정

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -1,13 +1,15 @@
-import type { NextPage } from "next";
-import Button from "@components/Button";
-import Input from "@components/Input";
-import Layout from "@components/Layout";
-import useUser from "@libs/client/useUser";
-import { useForm } from "react-hook-form";
-import { useEffect } from "react";
-import useMutation from "@libs/client/useMutation";
+import type { NextPage } from 'next';
+import Button from '@components/Button';
+import Input from '@components/Input';
+import Layout from '@components/Layout';
+import useUser from '@libs/client/useUser';
+import { useForm } from 'react-hook-form';
+import { useEffect } from 'react';
+import useMutation from '@libs/client/useMutation';
+import { useState } from 'react';
 
 interface EditProfileForm {
+  avatar?: FileList;
   email?: string;
   phone?: string;
   name?: string;
@@ -27,42 +29,62 @@ const EditProfile: NextPage = () => {
     handleSubmit,
     setError,
     formState: { errors },
+    watch,
   } = useForm<EditProfileForm>();
   const [editProfile, { data, loading }] =
     useMutation<EditProfileResponse>(`/api/users/me`);
 
-  const onValid = ({ email, phone, name }: EditProfileForm) => {
+  const [avatarPreview, setAvatarPreview] = useState('');
+  const avatar = watch('avatar');
+
+  const onValid = ({ email, phone, name, avatar }: EditProfileForm) => {
     if (loading) return;
     if (!email && !phone && !name) {
-      return setError("formErrors", {
-        message: "이메일 혹은 전화번호 중 하나를 입력해주세요.",
+      return setError('formErrors', {
+        message: '이메일 혹은 전화번호 중 하나를 입력해주세요.',
       });
     }
     editProfile({ email, phone, name });
   };
 
   useEffect(() => {
-    if (user?.email) setValue("email", user.email);
-    if (user?.phone) setValue("phone", user.phone);
-    if (user?.name) setValue("name", user.name);
+    if (user?.email) setValue('email', user.email);
+    if (user?.phone) setValue('phone', user.phone);
+    if (user?.name) setValue('name', user.name);
   }, [user, setValue]);
 
   useEffect(() => {
     if (data && !data.ok && data.error) {
-      return setError("formErrors", { message: data.error });
+      return setError('formErrors', { message: data.error });
     }
   }, [data, setError]);
+
+  useEffect(() => {
+    if (avatar && avatar.length > 0) {
+      const file = avatar[0];
+      setAvatarPreview(URL.createObjectURL(file));
+    }
+  }, [avatar]);
+
   return (
     <Layout canGoBack>
       <form onSubmit={handleSubmit(onValid)} className="px-4 py-10 space-y-4">
         <div className="flex items-center space-x-3">
-          <div className="w-14 h-14 bg-slate-400 rounded-full" />
+          {avatarPreview ? (
+            <img
+              src={avatarPreview}
+              className="w-14 h-14 bg-slate-400 rounded-full"
+            />
+          ) : (
+            <div className="w-14 h-14 bg-slate-400 rounded-full" />
+          )}
           <label
             htmlFor="picture"
             className="focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 px-3 py-2 text-sm font-semibold text-gray-700 border border-gray-300 rounded-md shadow-sm cursor-pointer"
           >
             Change
             <input
+              {...register('avatar')}
               id="picture"
               type="file"
               className="hidden"
@@ -71,21 +93,21 @@ const EditProfile: NextPage = () => {
           </label>
         </div>
         <Input
-          register={register("name")}
+          register={register('name')}
           type="text"
           inputId="name"
           label="Name"
           required={false}
         />
         <Input
-          register={register("email")}
+          register={register('email')}
           type="email"
           inputId="email"
           label="Email address"
           required={false}
         />
         <Input
-          register={register("phone")}
+          register={register('phone')}
           type="text"
           inputId="phone"
           label="Phone number"
