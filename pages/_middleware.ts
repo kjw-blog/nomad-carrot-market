2bf29dacc5a2144a7300f5cab519ab422b4a6a2d
kjw-blog <rkdwjddnr11@naver.com>
2022-05-26 14:31:03 +0900

    [#19 NEXTJS DEEP DIVE] 미들웨어로 유저가 봇인지, 로그인 쿠키가 있는지 체크

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,5 +1,24 @@
-import { NextFetchEvent, NextRequest } from 'next/server';
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log(req.ua);
+  if (req.ua?.isBot) {
+    // bot 접근 시 403 에러 발생
+    return new Response("Please don't be a bot. Be Human.", { status: 403 });
+  }
+
+  if (!req.url.includes('/api')) {
+    if (!req.url.includes('/enter') && !req.cookies.carrotsession) {
+      // 로그인 쿠키가 없을경우 엔터 페이지로 redirect
+      // 로그인 api 호출 시 redirect를 막기위해 api가 들어가는 url은 감시하지 않는다.
+
+      // useEffect로 enter 페이지로 이동하는것과 다르게
+      // 깜빡거림 없이 바로 enter 페이지로 이동시킨다.
+
+      return NextResponse.redirect('/enter');
+    }
+  }
+
+  console.log(req.geo?.country);
+  // 호스팅업체에서 제공하는것 ?
+  // 로컬환경에선 undefined
 }
